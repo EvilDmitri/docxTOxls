75e32345637e239b42438444e2402151d0b50f6b
EvilDmitri <dmitribrach@gmail.com>
2013-05-07 14:52:59 +0300

    Excel parser

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 import xlrd
 rb = xlrd.open_workbook('bhrec.xls', formatting_info=True)
 sheet = rb.sheet_by_index(0)
-for rownum in range(sheet.nrows):
+for rownum in range(2,sheet.nrows):
     row = sheet.row_values(rownum)
     for c_el in row:
         if c_el:
